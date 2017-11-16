8e3c3b462538c488e271ea3cae64de6ddbc3cf08
CatSoundsAwesome <wigfridwilson@gmail.com>
2017-11-16 14:39:03 -0800

    drop-down selection for static gradient and solid-fill are working

--- background.js
+++ background.js
@@ -1,16 +1,18 @@
-var body = document.querySelector('body');
+const html = document.querySelector('html');
+var backgroundForm = document.getElementById('background-choice');
 
+// Event listeners
+// ==================================================
+backgroundForm.addEventListener('change', getValue);
 
-body.style.background = "linear-gradient(" + getRandom(360) + "deg, rgb(" + randomRGB() + "), rgb(" + randomRGB() + ")";
-body.style.backgroundColor = "rgb(" + randomRGB() + ")";
-
-
+// Generates random gradient
+// ==================================================
 function getRandom(upper, lower=0){
 	var randomNum;
-	if(lower === 0){ //0-upper limit
+	if(lower === 0){ //range 0 to upper limit
 		randomNum = Math.random() * (upper + 1);
 		randomNum = Math.floor(randomNum);
-	} else{ //lower - upper range
+	} else{ //range lower to upper range
 		randomNum = Math.random() * (upper - lower) + lower;
 		randomNum = Math.round(randomNum);
 	}
@@ -21,7 +23,29 @@ function randomRGB(){
 	var r = getRandom(255);
 	var g = getRandom(255);
 	var b = getRandom(255);
-	// var rgb = r + ", " + g + ", " + b;
-	console.log(r + ", " + g + ", " + b);
+	// console.log(r + ", " + g + ", " + b);
 	return r + ", " + g + ", " + b;
 }
+
+
+// Sets background style based on drop-down selection
+// ==================================================
+function getValue(){
+	var backgroundSelection = backgroundForm.value;
+	setBackground(backgroundSelection);
+}
+
+function setBackground(style){
+	// TODO add animated gradient
+	// TODO change solid-fill to open color-picker and apply selection
+	if (style === "gradient") {
+		html.style.background = "linear-gradient(" + getRandom(360) + "deg, rgb(" + randomRGB() + "), rgb(" + randomRGB() + ")";
+	}else if (style === "solid-fill") {
+		html.style.background = "none";
+		html.style.backgroundColor = "rgb(" + randomRGB() + ")";
+	}else {
+		html.style.background = "none";
+		html.style.backgroundColor = "white";
+	}
+} 
+
